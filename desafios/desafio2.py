42b06963380a2dd08955f8436ac6e58d73d35a78
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-07-27 00:30:14 -0300

    Create desafio2.py

---
+++ desafios/desafio2.py
@@ -0,0 +1,7 @@
+dia = input ('Em que dia você nasceu?')
+
+mes = input ('Em que mês você nasceu?')
+
+ano = input ('Em que ano você nasceu')
+
+print ('Você nasceu em', dia, 'de', mes, 'de', ano, '. Correto?')
