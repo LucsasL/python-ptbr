1579c3e579c7aaaa14970cfb4904392e31b912c1
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-08-02 10:35:58 -0300

    Update desafio004.py

--- desafios/desafio004.py
+++ desafios/desafio004.py
@@ -1,3 +1,17 @@
 Content = input('Digite algo: ')
 
-print(type(Content))
+print('O tipo primitivo desse calor é ', type(Content))
+
+print('Só tem espaços? ', Content.isspace())
+
+print('É um número? ', Content.isnumeric())
+
+print('É alfabético? ', Content.isalpha())
+
+print('É alfanumérico? ', Content.isalnum())
+
+print('Está em maiúscula? ', Content.isupper())
+
+print('Está em minúsculas? ', Content.islower())
+
+print('Está capitalizada? ', Content.istitle())
