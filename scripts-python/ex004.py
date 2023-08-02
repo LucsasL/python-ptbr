61f33640e0b4e740d22cc056c63b16ee736bb493
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-08-02 10:02:28 -0300

    Melhorando o código

--- scripts-python/ex004.py
+++ scripts-python/ex004.py
@@ -4,4 +4,6 @@ N2 = int(input('Digite outro número: '))
 
 S = N1 + N2
 
-print('A soma entre ', N1, ' e ', N2, ' é igual a {}.'.format(S))
+# print('A soma entre ', N1, ' e ', N2, ' é igual a {}.'.format(S))
+
+print('A soma entre {} e {} é igual a {}'.format(N1, N2, S))
