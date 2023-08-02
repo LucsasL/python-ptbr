31ef14b2d10c2b5123ec1b4f421ec2368db20b69
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-08-02 10:00:18 -0300

    Update ex004.py

--- scripts-python/ex004.py
+++ scripts-python/ex004.py
@@ -1,7 +1,7 @@
-N1 = int (input('Digite um número: '))
+N1 = int(input('Digite um número: '))
 
-N2 = int (input('Digite outro número: '))
+N2 = int(input('Digite outro número: '))
 
 S = N1 + N2
 
-print('A soma entre os números é igual a {}!'.format(S))
+print('A soma entre ', N1, ' e ', N2, ' é igual a {}.'.format(S))
