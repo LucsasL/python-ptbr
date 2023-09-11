56e04b42b4f08fc46f6f3f40d5c7d30dd496d839
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 17:54:20 -0300

    Update desafio013.py

--- desafios/desafio013.py
+++ desafios/desafio013.py
@@ -1,4 +1,4 @@
 sal = float(input('Digite o salário: R$'))
 novSal = float(sal * 1.15)
 
-print('o novo salário passará a ser R${}.'.format(novSal))
+print('O salário que antes era R${:.2f}, passará a ser R${:.2f}.'.format(sal, novSal))
