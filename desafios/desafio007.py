179f764879eae82f634083528424ac1a1f3e60dc
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 16:44:20 -0300

    Update desafio007.py

--- desafios/desafio007.py
+++ desafios/desafio007.py
@@ -3,4 +3,6 @@ n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
 m = float((n1 + n2) / 2)
 
-print('>>> A média do aluno {} foi {}.'.format(nome, m))
+print('>>> Nota 1: {}\n>>> Nota 2: {}'.format(n1, n2))
+
+print('>>> A média do aluno {} foi {:.1f}.'.format(nome, m))
