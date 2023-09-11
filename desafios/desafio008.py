f19f346010b6b41f0b519345331406777d96171b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 16:58:35 -0300

    Update desafio008.py

--- desafios/desafio008.py
+++ desafios/desafio008.py
@@ -1,4 +1,19 @@
-m = int(input('Escreva uma medida [M]: '))
+m = float(input('Escreva uma medida [M]: '))
+km = m * (1 / 1000)
+hm = m * (1 / 100)
+dam = m * (1 / 10)
+dc = m * 10
 cm = m * 100
+mm = m * 1000
+
+print('>>> A medida escrita é de {}km.'.format(km))
+
+print('>>> A medida escrita é de {}hm.'.format(hm))
+
+print('>>> A medida escrita é de {}dam.'.format(dam))
+
+print('>>> A medida escrita é de {}dc.'.format(dc))
 
 print('>>> A medida escrita é de {}cm.'.format(cm))
+
+print('>>> A medida escrita é de {}mm.'.format(mm))
