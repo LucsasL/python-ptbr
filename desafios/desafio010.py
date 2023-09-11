2ae988ce372aa97580f5f990ef3fda1ce4635538
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 17:20:13 -0300

    Update desafio010.py

--- desafios/desafio010.py
+++ desafios/desafio010.py
@@ -1,4 +1,4 @@
 reais = float(input('Quantos reais você tem?: R$'))
 dol = float(reais / 5.20)
 
-print('Você pode ter US${}, com essa quantia de dinheiro.'.format(dol))
+print('Com R${:.2f} vocé pode comprar US${:.2f}.'.format(reais, dol))
