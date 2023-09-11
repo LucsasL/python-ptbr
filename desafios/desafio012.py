d29449f6b4a25c82545b3a4c27695601ac0985c7
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 17:44:35 -0300

    Update desafio012.py

--- desafios/desafio012.py
+++ desafios/desafio012.py
@@ -1,4 +1,4 @@
 prod = float(input('Qual o preço do produto?: R$'))
-prodDes = float(prod * 0.95)
+prodDes = prod * 0.95
 
-print('O produto passará a custar R${} com 5 por cento de desconto'.format(prodDes))
+print('O produto que antes custava R${:.2f}, passará a custar R${:.2f} com 5 por cento de desconto'.format(prod, prodDes))
