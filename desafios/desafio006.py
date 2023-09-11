7d48ac235f3cb67f20724f07e4e4dc317048c3ce
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-11 14:36:52 -0300

    Desafio 6 incompleto + Desafio 7

---
+++ desafios/desafio006.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número: '))
+
+dobN = int(n * 2)
+
+triN = int(n * 3)
+
+raizN = float((n * n) / 2)
+
+print('>>> O dobro do número digitado é: {}\n>>> O triplo do número digitado é: {}\n>>> A raiz quadrada do número é: {}'.format(dobN, triN, raizN))
---
+++ desafios/desafio007.py
@@ -0,0 +1,6 @@
+nome = str(input('Nome do aluno: '))
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+m = float((n1 + n2) / 2)
+
+print('>>> A média do aluno {} foi {}.'.format(nome, m))
