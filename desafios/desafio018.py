2f86dc4a6c7bfe2fea74fe3de341dbc190c29a8b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-12 16:22:21 -0300

    Esqueleto desafio 18

---
+++ desafios/desafio018.py
@@ -0,0 +1,9 @@
+ang = float(input('Digite um ângulo: '))
+
+sen = ang / 2
+
+cos = ang / 2
+
+tan = ang 
+
+print('No ângulo de {} o seno é igual a {}, o cosseno igual a {} e a tangente igual a {}.'.format(ang, sen, cos, tan))
