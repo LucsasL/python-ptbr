111eaa2121cf44d106f6743a571963f6d0b78e71
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-12 16:38:44 -0300

    desafio 16

--- desafios/desafio016.py
+++ desafios/desafio016.py
@@ -1,5 +1,7 @@
-from math import trunc
+#from math import trunc
+#num = float(input('Digite um número: '))
+#print('A parte inteira de {} é {}.'.format(num, trunc(num)))
 
 num = float(input('Digite um número: '))
 
-print('A parte inteira de {} é {}.'.format(num, trunc(num)))
+print('A parte inteira de {} é {}.'.format(num, int(num)))
