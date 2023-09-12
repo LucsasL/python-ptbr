9592707cea4ed5566790ad1af6e3673db97b65c8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-12 16:44:52 -0300

    Update desafio017.py

--- desafios/desafio017.py
+++ desafios/desafio017.py
@@ -2,6 +2,6 @@ CO = float(input('Qual o comprimento do cateto oposto?: '))
 
 CA = float(input('Qual o comprimento do cateto adjacente?: '))
 
-H = CA + CO
+H = (CA ** 2 + CO ** 2) ** (1/2)
 
-print('Em um triângulo retângulo com cateto oposto de {}cm e cateto adjacente de {}cm, a hipotenusa é de {}cm.'.format(CO, CA, H))
+print('Em um triângulo retângulo com cateto oposto de {}cm e cateto adjacente de {}cm, a hipotenusa é de {:.2f}cm.'.format(CO, CA, H))
