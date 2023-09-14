080681deba33577c48e1f63c939ffc33e6297c97
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-13 21:36:42 -0300

    Create desafio022.py

---
+++ desafios/desafio022.py
@@ -0,0 +1,9 @@
+n = input('Digite seu nome: ')
+
+print('>>> ', n.upper(), '\n')
+
+print('>>> ', n.lower(), '\n')
+
+print('>>> ', len(n) - n.count(' '), '\n')
+
+print('>>> ', len(n.split()), '\n')
