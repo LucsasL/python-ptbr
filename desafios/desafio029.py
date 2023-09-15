c90d962d3942fc036f837e34c22f956d86781933
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 14:05:51 -0300

    Correção de valores

--- desafios/desafio029.py
+++ desafios/desafio029.py
@@ -1,7 +1,7 @@
 vel = float(input('Digite a velocidade em que um carro está se locomovendo: '))
 
 if vel > 80:
-    div = (vel - 80) * 2
+    div = (vel - 80) * 7
     print('O carro está mutado! Page a dívida de R${:.2f}.'.format(div))
 else:
     print('O carro está dentro da regularidade.')
