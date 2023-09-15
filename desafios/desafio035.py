d630a4f7ecb6e897994e1b9657a97d5309526ade
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 15:51:51 -0300

    Update desafio035.py

--- desafios/desafio035.py
+++ desafios/desafio035.py
@@ -2,7 +2,7 @@ L1 = float(input('Digite o tamanho do primeiro lado: '))
 L2 = float(input('Digite o tamanho do segundo lado: '))
 L3 = float(input('Digite o tamanho do terceiri lado: '))
 
-if L1 > L2 + L3 and L2 > L1 + L3 and L3 > L1 + L2:
+if L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2:
     print('>>> Com as medidas dispostas, não é possível formar um triângulo.')
 else:
     print('>>> Com as medidas apresentadas, é possível formar um triângulo.')
