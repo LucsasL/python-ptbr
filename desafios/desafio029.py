52f857a618b4d1120f40dfd8fa9b948e5eb04b74
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 16:23:56 -0300

    Update desafio029.py

--- desafios/desafio029.py
+++ desafios/desafio029.py
@@ -2,6 +2,7 @@ vel = float(input('Digite a velocidade em que um carro está se locomovendo: '))
 
 if vel > 80:
     div = (vel - 80) * 7
-    print('O carro está mutado! Page a dívida de R${:.2f}.'.format(div))
+    print('>>> MULTADO! Você excedeu o limite permitido que é de 80Km/h')
+    print('Você deve pagar uma multa de R${:.2f}.'.format(div))
 else:
-    print('O carro está dentro da regularidade.')
+    print('>>> O carro está dentro da regularidade.')
