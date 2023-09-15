9ec43bf02ccda9ed4efd64cf5125d2316d262d5c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 14:04:52 -0300

    Create desafio029.py

---
+++ desafios/desafio029.py
@@ -0,0 +1,7 @@
+vel = float(input('Digite a velocidade em que um carro está se locomovendo: '))
+
+if vel > 80:
+    div = (vel - 80) * 2
+    print('O carro está mutado! Page a dívida de R${:.2f}.'.format(div))
+else:
+    print('O carro está dentro da regularidade.')
