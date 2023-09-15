ebe026621dfda875c4389a8c87d61cb23cdcca6d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 17:52:27 -0300

    Update desafio035.py

--- desafios/desafio035.py
+++ desafios/desafio035.py
@@ -1,8 +1,13 @@
+print('-=-' * 20)
+print('Analisador de triângulos')
+print('-=-' * 20)
+
 L1 = float(input('Digite o tamanho do primeiro lado: '))
 L2 = float(input('Digite o tamanho do segundo lado: '))
 L3 = float(input('Digite o tamanho do terceiri lado: '))
 
 if L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2:
-    print('>>> Com as medidas dispostas, não é possível formar um triângulo.')
+    print('>>> Com as medidas apresentadas, É POSSÍVEL FORMAR um triângulo.')
+
 else:
-    print('>>> Com as medidas apresentadas, é possível formar um triângulo.')
+    print('>>> Com as medidas dispostas, NÃO É POSSÍVEL FORMAR um triângulo.')
