6396a78e67f8e81bb991185a164815aea6b23f83
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-15 16:47:06 -0300

    Update desafio032.py

--- desafios/desafio032.py
+++ desafios/desafio032.py
@@ -1,8 +1,11 @@
-ano = int(input('Digite o ano atual: '))
+from datetime import date
 
-anobi = ano % 4
+ano = int(input('Digite o ano a ser analisado ou digite o número 0 para analisar o ano atual: '))
 
-if anobi == 0:
-    print('>>> O ano {} é um ano bissexto.'.format(ano))
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('>>> O ano {} é um ano BISSEXTO.'.format(ano))
 else:
-    print('>>> O ano {} não é um ano bissexto.'.format(ano))
+    print('>>> O ano {} NÃO é BISSEXTO.'.format(ano))
