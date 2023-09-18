41402889a3a444e9971b8b4719ba2837f6db944d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-17 21:12:36 -0300

    Update desafio028.py

--- desafios/desafio028.py
+++ desafios/desafio028.py
@@ -3,9 +3,14 @@ from time import sleep
 
 num = randint(0, 5) # faz o computador pensar
 
-print('-=-' * 20)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
-print('-=-' * 20)
+cores = {'limpa': '\033[m',
+         'bold': '\033[1m',
+         'vermelho': '\033[1;31m',
+         'verde': '\033[1;32m'}
+
+print('\033[1;35m-=-\033[m' * 20)
+print('{}Vou pensar em um número entre 0 e 5. Tente adivinhar...'.format(cores['bold']))
+print('\033[1;35m-=-\033[m' * 20)
 
 answer = int(input('Em que número eu pensei?: ')) # Jogador tenta adivinhar
 
@@ -13,6 +18,6 @@ print('PROCESSANDO...')
 sleep(1.5)
 
 if answer == num:
-    print('PARABÉNS! Você conseguiu me vencer!')
+    print('>>> {}PARABÉNS!{} Você conseguiu me vencer!'.format(cores['verde'], cores['limpa']))
 else:
-    print('GANHEI. Eu pensei no número {} e não no {}.'.format(num, answer))
+    print('{}GANHEI.{} Eu pensei no número {} e não no {}.'.format(cores['vermelho'], cores['limpa'],num, answer))
