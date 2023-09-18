ddf461b95a91f49f7d549ee3cbae28769bcd1d4b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-18 18:05:02 -0300

    Update desafio035.py

--- desafios/desafio035.py
+++ desafios/desafio035.py
@@ -6,8 +6,12 @@ L1 = float(input('Digite o tamanho do primeiro lado: '))
 L2 = float(input('Digite o tamanho do segundo lado: '))
 L3 = float(input('Digite o tamanho do terceiri lado: '))
 
+cores = {'limpa': '\033[m',
+         'verde': '\033[1;32m',
+         'vermelho': '\033[1;31m'}
+
 if L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2:
-    print('>>> Com as medidas apresentadas, É POSSÍVEL FORMAR um triângulo.')
+    print('>>> Com as medidas apresentadas, {}É POSSÍVEL FORMAR{} um triângulo.'.format(cores['verde'], cores['limpa']))
 
 else:
-    print('>>> Com as medidas dispostas, NÃO É POSSÍVEL FORMAR um triângulo.')
+    print('>>> Com as medidas dispostas, {}NÃO É POSSÍVEL FORMAR{} um triângulo.'.format(cores['vermelho'], cores['limpa']))
