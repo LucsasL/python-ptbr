e18c2ab4a5efb977ade3003494750b112e884356
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-17 21:20:20 -0300

    Update desafio030.py

--- desafios/desafio030.py
+++ desafios/desafio030.py
@@ -1,6 +1,9 @@
 num = int(input('Me diga um número qualquer: '))
 
+cores = {'amarelo': '\033[33m',
+         'limpa': '\033[m'}
+
 if num % 2 == 0:
-    print('O número {} é PAR.'.format(num))
+    print('O número {} é {}PAR.{}'.format(num, cores['amarelo'], cores['limpa']))
 else:
-    print('O número {} é ÍMPAR.'.format(num))
+    print('O número {} é {}ÍMPAR.{}'.format(num, cores['amarelo'], cores['limpa']))
