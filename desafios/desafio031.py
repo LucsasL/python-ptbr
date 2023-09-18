667603dde2334446dfa5f3bd971c44ed001e394f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-17 21:22:03 -0300

    Update desafio031.py

--- desafios/desafio031.py
+++ desafios/desafio031.py
@@ -1,8 +1,10 @@
 DisViag = float(input('Digite a distância da viagem [Km]: '))
 
+cores = {'amarelo': '\033[33m', 'limpa': '\033[m'}
+
 if DisViag <= 200:
     Pass = DisViag * 0.5
 else:
     Pass = DisViag * 0.45
 
-print('A passagem custará R${:.2f}.'.format(Pass))
+print('A passagem custará {}R${:.2f}.{}'.format(cores['amarelo'], Pass, cores['limpa']))
