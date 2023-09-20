c23bf9a3a97626bb9fb651b8f59a7005ad941c07
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-20 17:55:36 -0300

    Create desafio042.py

---
+++ desafios/desafio042.py
@@ -0,0 +1,28 @@
+forms = {'limpa': '\033[m',
+         'bold': '\033[1m',
+         'roxo': '\033[1;35m',
+         'azul': '\033[34m',
+         'verde': '\033[32m',
+         'vermelho': '\033[31m'}
+
+print('{}-=-{}'.format(forms['roxo'], forms['limpa']) * 20)
+print('{}Analisador de triângulos{}'.format(forms['bold'], forms['limpa']))
+print('{}-=-{}'.format(forms['roxo'], forms['limpa']) * 20)
+
+L1 = float(input('Digite o tamanho do primeiro lado: '))
+L2 = float(input('Digite o tamanho do segundo lado: '))
+L3 = float(input('Digite o tamanho do terceiri lado: '))
+
+print('')
+
+if L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2 and L1 == L2 and L2 == L3:
+    print('>>> Com as medidas apresentadas, {}É POSSÍVEL FORMAR{} UM {}TRIÂNGULO EQUILÁTERO.{}'.format(forms['verde'], forms['limpa'], forms['azul'], forms['limpa']))
+
+elif L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2 and L1 == L2 or L2 == L3 or L1 == L3:
+    print('>>> Com as medidas apresentadas, {}É POSSÍVEL FORMAR{} UM {}TRIÂNGULO ISÓSCELES.{}'.format(forms['verde'], forms['limpa'], forms['azul'], forms['limpa']))
+
+elif L1 < L2 + L3 and L2 < L1 + L3 and L3 < L1 + L2 and L1 != L2 and L2 != L3 and L3 != L1:
+    print('>>> Com as medidas apresentadas, {}É POSSÍVEL FORMAR{} UM {}TRIÂNGULO ESCALENO.{}'.format(forms['verde'], forms['limpa'], forms['azul'], forms['limpa']))
+
+else:
+    print('>>> Com as medidas dispostas, {}NÃO É POSSÍVEL FORMAR{} um triângulo.'.format(forms['vermelho'], forms['limpa']))
