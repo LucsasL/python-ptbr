9e41fa81873d3d343f1f6b57377ddec94c49db35
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-20 03:58:56 -0300

    Desafio 039 + Criação desafio 040

---
+++ desafios/desafio039.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+anonasc = int(input('Digite o ano de nascimento: '))
+
+idade = date.today().year - anonasc
+
+if idade > 18:
+    tempo = idade - 18
+    print('Já se passaram {} anos desde que você devia se alistar no exército.'.format(tempo))
+elif idade == 18:
+    print('Já é hora de se alistar no exército.')
+else:
+    tempo = 18 - idade
+    print('Ainda há de se esperar {} anos para se alistar no exército.'.format(tempo))
