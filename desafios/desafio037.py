4e60c7473362994ded98d8503ee7405ad4aea06c
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-21 03:17:15 -0300

    Update desafio037.py

--- desafios/desafio037.py
+++ desafios/desafio037.py
@@ -1,6 +1,7 @@
 forms = {'limpa': '\033[m',
          'bold': '\033[1m',
-         'roxo': '\033[1;35m'}
+         'roxo': '\033[1;35m',
+         'azul': '\033[34m'}
 
 print('{}-=-{}'.format(forms['roxo'], forms['limpa']) * 20)
 print('{}Conversor de números{}'.format(forms['bold'], forms['limpa']))
@@ -8,17 +9,19 @@ print('{}-=-{}'.format(forms['roxo'], forms['limpa']) * 20)
 
 num = int(input('Digite um número inteiro: '))
 
-print('')
-print('[1] Converter para {}Binário{}'.format(forms['bold'], forms['limpa']))
-print('[2] Converter para {}Octal{}'.format(forms['bold'], forms['limpa']))
-print('[3] Converter para {}Hexadecimal{}'.format(forms['bold'], forms['limpa']))
+print('''
+[1] Converter para {}BINÁRIO{}
+[2] Converter para {}OCTAL{}
+[3] Converter para {}HEXADECIMAL{}'''.format(forms['bold'], forms['limpa'], forms['bold'], forms['limpa'], forms['bold'], forms['limpa']))
 
 print('')
 escolha = int(input('Qual será a base de conversão?: '))
 
 if escolha == 1:
-    print('teste')
+    print('>>> {} convertido para {}BINÁRIO{} é igual a {}'.format(num, forms['azul'], forms['limpa'], bin(num)[2:]))
 elif escolha == 2:
-    print('teste 2')
+    print('>>> {} convertido para {}OCTAL{} é igual a {}'.format(num, forms['azul'], forms['limpa'], oct(num)[2:]))
+elif escolha == 3:
+    print('>>> {} convertido para {}HEXADECIMAL{} é igual a {}'.format(num, forms['azul'], forms['limpa'], hex(num)[2:]))
 else:
-    print('teste 3')
+    print('>>> Opção inválida. Tente novamente.')
