f31603d0cf087a6a2aeb1d8de47c22088d6ce3b2
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-21 03:25:18 -0300

    Update desafio039.py

--- desafios/desafio039.py
+++ desafios/desafio039.py
@@ -6,9 +6,9 @@ idade = date.today().year - anonasc
 
 if idade > 18:
     tempo = idade - 18
-    print('Já se passaram {} anos desde que você devia se alistar no exército.'.format(tempo))
+    print('Já se passaram {} anos desde que você devia se alistar no exército, que foi {}.'.format(tempo, (anonasc + 18)))
 elif idade == 18:
     print('Já é hora de se alistar no exército.')
 else:
     tempo = 18 - idade
-    print('Ainda há de se esperar {} anos para se alistar no exército.'.format(tempo))
+    print('Ainda há de se esperar {} anos para se alistar no exército, que será em {}.'.format(tempo, (anonasc + 18)))
