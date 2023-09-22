e948508ded9c9a6969a4bb11609412f507fc2fa7
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-22 04:02:59 -0300

    Complemento

--- desafios/mod2/desafio054.py
+++ desafios/mod2/desafio054.py
@@ -1,11 +1,21 @@
 from datetime import date
 
+contmai = 0
+
 for c in range(1, 8):
-    anonasc = int(input('Digite o ano de nascimento: '))
+    anonasc = int(input('>>> Digite o ano de nascimento: '))
 
     idade = date.today().year - anonasc
 
     if idade >= 18:
-        print('Está na MAIORIDADE')
+        print('... Está na MAIORIDADE')
+        contmai += 1
     else:
-        print('NÃO está na MAIORIDADE')
+        print('... NÃO está na MAIORIDADE')
+
+if contmai == c:
+    print('... Todas as pessoas listadas estão na maioridade.')
+elif contmai > 0 and contmai < c:
+    print('... Das pessoas digitadas, apenas {} está(ão) na maioridade'.format(contmai))
+else:
+    print('... Ninguém listado está na maioridade.')
