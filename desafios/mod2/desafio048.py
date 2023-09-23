ea76ed4a42fb3929f3598173c47ff32370da0344
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-23 01:16:36 -0300

    Update desafio048.py

--- desafios/mod2/desafio048.py
+++ desafios/mod2/desafio048.py
@@ -1,8 +1,11 @@
 s = 0
 
-for c in range(0, 500):
-    if c % 2 == 1 and c // 3 == 0:
-        s = s + c
-        print(c)
+cont = 0
 
-print('O resultado das somas é igual a: {}.'.format(s))
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1
+        s = s + c
+        print(c, end=', ')
+
+print('\n... O resultado da soma dos {} números ímpares é igual a: {}.'.format(cont, s))
