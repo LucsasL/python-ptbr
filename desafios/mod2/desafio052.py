92ca743b4149726e6d18176e6efd7ef67c7aa640
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-23 02:05:56 -0300

    Update desafio052.py

--- desafios/mod2/desafio052.py
+++ desafios/mod2/desafio052.py
@@ -1,4 +1,19 @@
-for c in range(1, 10):
-    num = int(input('Digite o '))
-    if num / c != 0:
-        print('lol')
+num = int(input('Digite um número: '))
+
+tot = 0
+
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[32m', end=' ')
+        tot += 1
+    else:
+        print('\033[31m', end=' ')
+        
+    print(c, end='\033[m ')
+
+print('\n... Existem {} divsíveis para o número {}.'.format(tot, num), end='')
+
+if tot == 2:
+    print(' Sendo assim um NÚMERO PRIMO.')
+else:
+    print(' NÃO é um NÚMERO PRIMO.')
