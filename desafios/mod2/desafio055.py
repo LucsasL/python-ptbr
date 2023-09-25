a0303adf99e21578f81622f27e467b9af7e7b9bd
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-25 08:02:43 -0300

    Update desafio055.py

--- desafios/mod2/desafio055.py
+++ desafios/mod2/desafio055.py
@@ -1,14 +1,18 @@
 maior = 0
 
-menor = 1000000000
+menor = 0
 
-for c in range(0, 6):
-    peso = float(input('Digite o peso [Kg]: '))
+for c in range(1, 6):
+    peso = float(input('Digite o peso da {}° pessoa [Kg]: '.format(c)))
 
-    if peso > maior:
-        maior = peso
-    elif peso < menor:
-        menor = peso
+    if c == 1:
+        maior = c
+        menor = c
+    else:
+        if peso > maior:
+            maior = peso
+        elif peso < menor:
+            menor = peso
 
 print('... MAIS PESADO: {:.2f}Kg'.format(maior))
 print('... MENOS PESADO: {:.2f}Kg'.format(menor))
