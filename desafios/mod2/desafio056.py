c2eced729321367f527c6a2c1ebb1d627689c0e8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-25 08:19:14 -0300

    Update desafio056.py

--- desafios/mod2/desafio056.py
+++ desafios/mod2/desafio056.py
@@ -4,17 +4,13 @@ mm20 = 0
 
 maioridade = 0
 
-nomem = str
-
 for c in range(1, 5):
+    print('--- {}° PESSOA ---'.format(c))
     nome = str(input('\n>>> Digite o nome da {}° pessoa: '.format(c)))
     idade = int(input('>>> Digite a idade da {}° pessoa: '.format(c)))
     sexo = str(input('>>> Digite o sexo da {}° pessoa [M / F]: '.format(c)).upper())
 
     midade += idade
-
-    if idade > maioridade:
-        maioridade = idade
 
     if sexo == 'M' and idade > maioridade:
         nomem = nome
@@ -22,8 +18,10 @@ for c in range(1, 5):
     if sexo == 'F' and idade < 20:
         mm20 += 1
 
+    maioridade = idade
+
 midade = midade / c
 
 print('\n... MÉDIA DE IDADE ENTRE AS PESSOAS: {}'.format(midade))
-print('... NOME DO HOMEM MAIS VELHO: {}'.format(nomem))
+print('... NOME DO HOMEM MAIS VELHO TEM {} ANOS E É O {}'.format(maioridade, nomem))
 print('... QUANTIDADE DE MULHERES COM MENOS DE 20 ANOS: {}'.format(mm20))
