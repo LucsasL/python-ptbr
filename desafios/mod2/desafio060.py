239bccafea410db74d630e935b6c248ce5fefcab
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-28 03:42:27 -0300

    Update desafio060.py

--- desafios/mod2/desafio060.py
+++ desafios/mod2/desafio060.py
@@ -2,17 +2,13 @@ num = int(input('Digite um número para calcular seu fatorial: '))
 
 proc = num - 1
 
-resp = 'S'
-
 fat = num
 
 print('Calculando ', num, '! = ', num, end=' x ')
 
-while proc != 1:
-    fat = fat * proc
-    print(proc, ' x ', end='')
+while proc > 0:
+    fat *= proc
+    print(proc, ' x ' if proc > 1 else ' = {}'.format(fat), end='')
     proc -= 1
 
-print('1 = ', fat)
-
-print('...  O fatorial de {} é igual a {}.'.format(num, fat))
+print('\n...  O fatorial de {} é igual a {}.'.format(num, fat))
