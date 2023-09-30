adf37e9341a3170b6e0a130cc32bb5b079000e8f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-09-30 14:22:22 -0300

    Consegui

--- desafios/mod2/desafio062.py
+++ desafios/mod2/desafio062.py
@@ -2,30 +2,30 @@ print('=' * 30)
 print('{:^30}'.format('SUPER POGRESSÃO ARITMÉTICA'))
 print('=' * 30)
 
-primeiro = int(input('>>> Digite o primeiro termo: '))
+primeiro = int(input('Digite o primeiro termo: '))
 
-raz = int(input('>>> Digite a razão: '))
+raz = int(input('Digite a razão: '))
 
 termo = primeiro
 
 cont = 1
 
-add = 1
+total = 0
+
+add = 10
 
 while add != 0:
-    if cont == 1:
-        while cont <= 10:
-            print('{} > '.format(termo), end='')
-            termo += raz
-            cont += 1
-    else:
-        add = int(input('>>> Quantos termos a mais quer adicionar?: '))
+    total += add
+    
+    while cont <= total:
+        print('{} > '.format(termo), end='')
+        termo += raz
+        cont += 1
 
-        while add == cont:
-            print('{} > '.format(termo), end='')
-            termo += raz
-            cont += 1
+    print('PAUSA')
 
-    print('FIM')
+    add = int(input('Quantos termos você quer mostrar a mais?: '))
+
+print('\n... Foram mostrados {} termos no total.'.format(cont))
 
 print('\n... Finalizando programa...')
