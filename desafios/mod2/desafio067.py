7b40b09ff590eda5734da97cd8814af0ba493b36
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-01 10:09:01 -0300

    Create desafio067.py

---
+++ desafios/mod2/desafio067.py
@@ -0,0 +1,16 @@
+mult = 1
+
+while True:
+    print('-' * 15)
+    num = int(input('>>> Digite o número a ser multiplicado [número negativo para parar]: '))
+    print('-' * 15)
+    if num >= 0:
+        while mult <= 10:
+            print(f'{num} X {mult:2} = {num * mult}')
+            mult += 1
+    mult = 1
+
+    if num < 0:
+        break
+
+print('\nFINALIZANDO PROGRAMA...')
