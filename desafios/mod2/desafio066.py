80a6e7d8227882839af66d8c5df1f3cb7ed80828
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-01 09:50:32 -0300

    Create desafio066.py

---
+++ desafios/mod2/desafio066.py
@@ -0,0 +1,12 @@
+cont = 0
+
+s = 0
+
+while True:
+    num = int(input(f'>>> Digite o {cont + 1}° número [999 para parar]: '))
+    if num == 999:
+        break
+    cont += 1
+    s += num
+
+print(f'\n... Foram digitados {cont} números durante a execução do programa. A soma de todos eles foi {s}.')
