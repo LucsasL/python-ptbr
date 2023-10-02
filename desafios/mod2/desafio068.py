2c5f27490ed08c365773951dcd20e832abf2ddd0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-02 06:22:48 -0300

    Desafio 69

--- desafios/mod2/desafio068.py
+++ desafios/mod2/desafio068.py
@@ -15,7 +15,7 @@ print('{}-=-{}'.format(forms['roxo'], forms['limpa']) * 20)
 while True:
     jog = int(input('\n>>> Digite um número: '))
 
-    POI = str(input('>>> PAR ou ÍMPAR [P / I]: ')).upper()[0].strip()
+    PoI = str(input('>>> PAR ou ÍMPAR [P / I]: '))[0].upper().strip()
 
     comp = randint(1, 20)
 
@@ -26,48 +26,56 @@ while True:
     else:
         Res = '\033[34mÍMPAR\033[m'
 
-    if POI == 'P' and s % 2 == 0:
+    if PoI == 'P' and s % 2 == 0:
         jogven += 1
 
         print(f'\n... O computador jogou {comp} e você jogou {jog}, somando os números jogados temos {s}, que é {Res}.')
 
         print('\n{}Você venceu!{} Vamos jogar novamente...'.format(forms['verde'], forms['limpa']))
 
-    elif POI == 'I' and s % 2 == 1:
+    elif PoI == 'I' and s % 2 == 1:
         jogven += 1
 
         print(f'\n... O computador jogou {comp} e você jogou {jog}, somando os números jogados temos {s}, que é {Res}.')
 
         print('\n{}Você venceu!{} Vamos jogar novamente...'.format(forms['verde'], forms['limpa']))
 
-    elif POI == 'P' and s % 2 == 1:
+    elif PoI == 'P' and s % 2 == 1:
         break
 
-    elif POI == 'I' and s % 2 == 0:
+    elif PoI == 'I' and s % 2 == 0:
         break
 
     else:
-        while POI not in 'PI':
-            POI = str(input('Valor inválido! Digite novamente: '))
+        if PoI == 'P' and s % 2 == 1:
+            break
 
-            if POI == 'P' and s % 2 == 0:
+        elif PoI == 'I' and s % 2 == 0:
+            break
+
+        while PoI not in 'PI':
+            PoI = str(input('Valor inválido! Digite novamente [P / I]: ')).upper()[0].strip()
+
+            s = jog + comp
+
+            if PoI == 'P' and s % 2 == 0:
                 jogven += 1
 
                 print(f'\n... O computador jogou {comp} e você jogou {jog}, somando os números jogados temos {s}, que é {Res}.')
 
                 print('\n{}Você venceu!{} Vamos jogar novamente...'.format(forms['verde'], forms['limpa']))
 
-            elif POI == 'I' and s % 2 == 1:
+            elif PoI == 'I' and s % 2 == 1:
                 jogven += 1
 
                 print(f'\n... O computador jogou {comp} e você jogou {jog}, somando os números jogados temos {s}, que é {Res}.')
 
                 print('\n{}Você venceu!{} Vamos jogar novamente...'.format(forms['verde'], forms['limpa']))
 
-            elif POI == 'P' and s % 2 == 1:
+            elif PoI == 'P' and s % 2 == 1:
                 break
 
-            elif POI == 'I' and s % 2 == 0:
+            elif PoI == 'I' and s % 2 == 0:
                 break
 
 print(f'\n... O computador jogou {comp} e você jogou {jog}, somando os números jogados temos {s}, que é {Res}.')
---
+++ desafios/mod2/desafio069.py
@@ -0,0 +1,49 @@
+print('=' * 30)
+print('ANALISE DE DADOS')
+print('=' * 30)
+
+resp = 'DEFINED'
+
+pmaior = nump = h = mm20 = 0
+
+while True:
+    # Quebra de laço caso haja erro de digitação e não haja mais pessoas a cadastrar
+    if resp == 'N':
+        break
+    
+    nump += 1
+
+    print(f'{nump}° PESSOA')
+    print('-' * 30)
+
+    # Formulário de pergunta
+    idade = int(input('>>> Digite a idade: '))
+
+    sexo = str(input('>>> Digite o sexo [M / F]: ')).upper()[0].strip()
+
+    resp = str(input('>>> Adicionar mais pessoas? [S / N]: ')).upper()[0].strip()
+    
+    # Calcula parâmetros
+    if idade > 18:
+        pmaior += 1
+
+    if idade < 20 and sexo == 'F':
+        mm20 += 1
+
+    if sexo == 'M':
+        h += 1
+
+    # Testa possibilidade de erro ao digitar
+    if resp == 'N':
+        break
+    elif resp != 'S':
+        if resp == 'N':
+            break
+
+        while resp not in 'SN':
+            resp = str(input('>>> Opção inválida! Digite novamente [S / N]: ')).upper()[0].strip()
+
+            if resp == 'N':
+                break
+
+print('''\n... Dos dados digitados, podemos concluir que {} pesssoas tem mais de 18 anos, {} pessoas são homens e {} das mulheres digitadas tem menos de 20 anos.'''.format(pmaior, h, mm20))
