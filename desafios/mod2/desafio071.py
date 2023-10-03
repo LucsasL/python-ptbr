60e1ae2a5953fa1185fbdcf979bc8f2383feeb8d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-03 17:29:19 -0300

    Update desafio071.py

--- desafios/mod2/desafio071.py
+++ desafios/mod2/desafio071.py
@@ -1,6 +1,21 @@
+unsed = uni = dezsed = dez = cinsed = cin = censed = cen = 0
+
 print('=' * 35)
 print('{:^35}'.format(' SACAR NO BANCO '))
 print('=' * 35)
 
 din = int(input('>>> Digite o valor a sacar: R$'))
 
+while unsed == uni:
+    unsed += 1
+
+while dezsed == dez:
+    dezsed += 1
+
+while cinsed == cin:
+    cinsed += 1
+
+while censed == cen:
+    censed += 1
+
+print('Foram entregues {} ce')
