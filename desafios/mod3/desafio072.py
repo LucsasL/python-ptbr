2e616a3e7337a91b3ecb012e81801aae136167e8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-05 20:04:50 -0300

    Create desafio072.py

---
+++ desafios/mod3/desafio072.py
@@ -0,0 +1,13 @@
+números = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+dig = int(input('>>> Digite um número entre 0 e 20: '))
+
+if dig <= 20 or dig < 0:
+    print(f'O número digitado foi {números[dig]}')
+else:
+    while True:
+        dig = int(input('>>> Número inválido! Digite novamente: '))
+
+        if dig <= 20 and dig >= 0:
+            print(f'O número digitado foi {números[dig]}')
+            break
