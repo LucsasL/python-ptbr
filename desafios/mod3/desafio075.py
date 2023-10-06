4e642d5ec79de51f02d823eb0dd7a5bc17b2d9bd
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-06 14:38:14 -0300

    Desafio 75 quase concluído

--- desafios/mod3/desafio075.py
+++ desafios/mod3/desafio075.py
@@ -1,10 +1,20 @@
+par = nove = 0
+
 for cont in range(0, 4):
     valor = float(input('>>> Digite um número: '))
 
+    if valor == 9:
+        nove += 1
+
+    if valor % 2 == 0:
+        par += 1
+
     valores = (valor, valor, valor, valor)
 
-print(f'... O número 9 apareceu {valores} vezes.')
+print(f'... Você digitou os valores: {valores}')
 
-print(f'... O primeiro 3 foi digitado na posição {valores}.')
+print(f'... O número 9 apareceu {nove} vezes.')
 
-print(f'... Os números pares digitados foram: {valores}')
+print(f'... O primeiro 3 foi digitado na posição {valores.find(3)}.')
+
+print(f'... Os números pares digitados foram: {par}')
