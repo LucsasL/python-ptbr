8b93d0c41ead1d923ed857331dbf9e8d4e69e0ee
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-06 16:20:28 -0300

    Update desafio072.py

--- desafios/mod3/desafio072.py
+++ desafios/mod3/desafio072.py
@@ -1,13 +1,30 @@
 números = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-dig = int(input('>>> Digite um número entre 0 e 20: '))
+resp = ' '
 
-if dig <= 20 and dig >= 0:
-    print(f'O número digitado foi {números[dig]}.')
-else:
-    while True:
-        dig = int(input('>>> Número inválido! Digite um número entre 0 e 20: '))
+while resp != 'N':
+    dig = int(input('>>> Digite um número entre 0 e 20: '))
 
-        if dig <= 20 and dig >= 0:
-            print(f'O número digitado foi {números[dig]}.')
+    if 0 <= dig <= 20:
+        print(f'... O número digitado foi {números[dig]}.')
+
+        resp = str(input('>>> Quer digitar mais um número [S / N]: ')).strip().upper()[0]
+
+        if resp == 'N':
+            break
+    else:
+        while True:
+            dig = int(input('>>> Número inválido! Digite um número entre 0 e 20: '))
+
+            if dig <= 20 and dig >= 0:
+                print(f'... O número digitado foi {números[dig]}.')
+
+                resp = str(input('>>> Quer digitar mais um número [S / N]: ')).strip().upper()[0]
+
+                if resp == 'N':
+                    break
+                else:
+                    break
+
+        if resp == 'N':
             break
