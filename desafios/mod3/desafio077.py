4d875624fcd2a8e3f7054f60c6072d1b1123a39f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-06 15:13:28 -0300

    Create desafio077.py

---
+++ desafios/mod3/desafio077.py
@@ -0,0 +1,7 @@
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+
+vogais = ('a', 'e', 'i', 'o', 'u')
+
+for cont in range(0, 11):
+    print(f'Na palavra {palavras[cont]} temos {vogais}')
+    cont += 1
