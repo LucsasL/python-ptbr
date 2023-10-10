c3e44619b9c4de525f4393e7fc3a58c7da63be6b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-10 19:57:08 -0300

    Update ex016.py

--- scripts-python/ex016.py
+++ scripts-python/ex016.py
@@ -1,20 +1,9 @@
-# Diferentemente das Tuplas, as listas são facilmente mutáveis
-num = [2, 5, 9, 1]
+a = [2, 3, 4, 7]
 
-num[2] = 3
-num.append(7)
-num.sort()
-num.insert(2, 0)
+b = a[:]
 
-print(num)
+b[2] = 8
 
-# Podemos também inverter a ordem que a lista é mostrada
-num.sort(reverse=True)
-print(num)
+print(f'Lista A: {a}')
 
-print(f'\n... Essa lista tem {len(num)} elementos.')
-
-# Podemos eliminar elementos também
-num.pop()
-
-print(num)
+print(f'Lista B: {b}')
