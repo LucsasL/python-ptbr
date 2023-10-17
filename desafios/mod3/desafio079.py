20da403d20763c2d6d641c1b235b39646de51d96
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-16 21:35:01 -0300

    Update desafio079.py

--- desafios/mod3/desafio079.py
+++ desafios/mod3/desafio079.py
@@ -6,11 +6,35 @@ forms = {
 
 numeros = list()
 
+c = 0
+
 print(f'{forms["roxo"]}-{forms["limpa"]}' * 50)
 print(f'{forms["bold"]}TESTE DE DESAFIO{forms["limpa"]}'.center(55))
 print(f'{forms["roxo"]}-{forms["limpa"]}' * 50)
 
-for c in range(0, 5):
-    numeros.append(int(input(f'>>> Digite o {c + 1} número: ')))
+while True:
+    numeros.append(int(input(f'>>> Digite o {c + 1}° número: ')))
 
-print(numeros)
+    if numeros[c] == numeros[c - 1]:
+        print(f'... Número repetido! Não vou adicionar...')
+        resp = str(input('>>> Adicionar mais um número? [S / N]:')).strip().upper()
+
+    c += 1
+
+    resp = str(input('>>> Adicionar mais um número? [S / N]:')).strip().upper()[0]
+
+    if resp != 'S' and resp != 'N':
+        while True:
+            resp = str(input('>>> Valor inválido! Adicionar mais um número? [S / N]:')).strip().upper()
+
+            if resp == 'N' or resp == 'S':
+                break
+
+        if resp == 'N':
+            break
+
+    elif resp == 'N':
+        break
+
+print('-=' * 20)
+print(f'Você digitou os números: {numeros}')
