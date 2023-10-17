7fd988838cbb19e8db0e3681fdc7617bd9589cc8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-16 21:41:55 -0300

    Update desafio079.py

--- desafios/mod3/desafio079.py
+++ desafios/mod3/desafio079.py
@@ -9,19 +9,19 @@ numeros = list()
 c = 0
 
 print(f'{forms["roxo"]}-{forms["limpa"]}' * 50)
-print(f'{forms["bold"]}TESTE DE DESAFIO{forms["limpa"]}'.center(55))
+print(f'{forms["bold"]}DESAFIO CONTADOR DE LISTA{forms["limpa"]}'.center(55))
 print(f'{forms["roxo"]}-{forms["limpa"]}' * 50)
 
 while True:
     numeros.append(int(input(f'>>> Digite o {c + 1}° número: ')))
 
-    if numeros[c] == numeros[c - 1]:
+    if c > 0 and numeros[c] == numeros[:]:
         print(f'... Número repetido! Não vou adicionar...')
         resp = str(input('>>> Adicionar mais um número? [S / N]:')).strip().upper()
+    else:
+        resp = str(input('>>> Adicionar mais um número? [S / N]:')).strip().upper()[0]
 
     c += 1
-
-    resp = str(input('>>> Adicionar mais um número? [S / N]:')).strip().upper()[0]
 
     if resp != 'S' and resp != 'N':
         while True:
