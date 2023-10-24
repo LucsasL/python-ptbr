23db6c4fcf2dd9802dc313d7cb1c84425bed31c3
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-24 20:20:51 -0300

    Desenvolvendo base do desafio 18

---
+++ desafios/mod3/desafio080.py
@@ -0,0 +1,11 @@
+forms = {
+    'limpa': '\033[m',
+    'bold': '\033[1m',
+    'roxo': '\033[1;35m',
+}
+
+print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
+print('Desafio 80'.center(60))
+print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
+
+# Estrutura básica
