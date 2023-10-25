e521dfc5d60990654c7901b4cee846c22f294bde
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-24 21:12:16 -0300

    Update desafio078.py

--- desafios/mod3/desafio078.py
+++ desafios/mod3/desafio078.py
@@ -8,16 +8,31 @@ for cont in range(0,5):
 
     if números[cont] > maior:
         maior = números[cont]
-        posmai = cont
+        posmai.append(números[cont])
+        
     
     if números[cont] < menor:
         menor = números[cont]
-        posmen = cont
+        posmen.append(números[cont])
 
 print('-' * 50)
 
 print(f'Você digitou os valores {números}')
 
-print(f'... O maior número foi o {maior} na posição {posmai[0]}... {posmai[1]}')
+if len(posmai) == 1:
+    print(f'... O maior número foi o {maior} na posição {posmai[0]}')
 
-print(f'... O menor número foi o {menor} na posição {posmen[0]}... {posmen[1]}')
+else:
+    print(f'... O maior número foi o {maior} na posição ', end='')
+
+    for pos in posmai:
+        print(f'{posmai[pos]}... ', end='' if posmai[:pos - 1] else '\n')
+
+if len(posmen) == 1:
+    print(f'... O menor número foi o {menor} na posição {posmen[0]}')
+
+else:
+    print(f'... O menor número foi o {menor} na posição ', end='')
+
+    for pos in posmen:
+        print(f'{posmen[pos]}... ', end='' if posmen[:pos - 1] else '\n')
