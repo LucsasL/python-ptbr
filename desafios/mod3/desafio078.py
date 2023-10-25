1ad158ecd50d450fe04b36fa9c104475d35995f8
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-25 06:49:25 -0300

    Update desafio078.py

--- desafios/mod3/desafio078.py
+++ desafios/mod3/desafio078.py
@@ -1,4 +1,6 @@
-posmai = posmen = números = list()
+números = []
+
+posmai = posmen = 0
 
 for cont in range(0,5):
     números.append(int(input(f'>>> Digite o valor da posição {cont}: ')))
@@ -6,33 +8,29 @@ for cont in range(0,5):
     if cont == 0:
         maior = menor = números[cont]
 
-    if números[cont] > maior:
-        maior = números[cont]
-        posmai.append(números[cont])
+    else:
+        if números[cont] > maior:
+            maior = números[cont]
+            posmai += 1
         
-    
-    if números[cont] < menor:
-        menor = números[cont]
-        posmen.append(números[cont])
+        if números[cont] < menor:
+            menor = números[cont]
+            posmen += 1
 
 print('-' * 50)
 
 print(f'Você digitou os valores {números}')
 
-if len(posmai) == 1:
-    print(f'... O maior número foi o {maior} na posição {posmai[0]}')
+print(f'... O maior número foi o {maior} nas posições ', end='')
 
-else:
-    print(f'... O maior número foi o {maior} na posição ', end='')
+for c, v in enumerate(números):
+    if v == maior:
+        print(f'{c}... ', end='')
 
-    for pos in posmai:
-        print(f'{posmai[pos]}... ', end='' if posmai[:pos - 1] else '\n')
+print('\n')
 
-if len(posmen) == 1:
-    print(f'... O menor número foi o {menor} na posição {posmen[0]}')
+print(f'... O menor número foi o {menor} nas posições ', end='')
 
-else:
-    print(f'... O menor número foi o {menor} na posição ', end='')
-
-    for pos in posmen:
-        print(f'{posmen[pos]}... ', end='' if posmen[:pos - 1] else '\n')
+for c, v in enumerate(números):
+    if v == menor:
+        print(f'{c}... ', end='')
