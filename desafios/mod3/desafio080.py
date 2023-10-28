ca0eb5af8ea823f3b8638cf6cf8f798b8bdcf9ab
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-27 22:19:55 -0300

    Update desafio080.py

--- desafios/mod3/desafio080.py
+++ desafios/mod3/desafio080.py
@@ -4,7 +4,7 @@ forms = {
     'roxo': '\033[1;35m',
 }
 
-numeros = [0, 0, 0, 0, 0]
+numeros = []
 
 print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 print('Desafio 80'.center(60))
@@ -12,13 +12,20 @@ print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 
 for c in range(0, 5):
     num = float(input(f'>>> Digite o {c + 1}° número número: '))
-    numeros.append(num)
 
-    if numeros[c] < numeros[c - 1]:
-        numeros.insert[0]
+    if c == 0 or num > numeros[-1]:
+        print('... Número adicionado na posição final da lista...')
+        numeros.append(num)
     
-    if numeros[c] > numeros[c - 1]:
-        numeros.insert[4]
+    else:
+        if num < numeros[0]:
+            pos = 0
+            while pos < len(numeros):
+                if num <= numeros[pos]:
+                    print(f'... Número posicionado no índice {pos} da lista...')
+                    numeros.insert(pos, num)
+                    break
+                pos += 1
 
-
-print(numeros)
+print('-=-' * 30)
+print(f'... Os valores digitados em ordem foi {numeros:.2f}.')
