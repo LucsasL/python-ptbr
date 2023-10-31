e1205738e9ac80a5c85d4fa88ba9b3bd2c2f83bf
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-31 12:24:10 -0300

    Desafio 82

--- desafios/mod3/desafio082.py
+++ desafios/mod3/desafio082.py
@@ -1,30 +1,21 @@
 forms = {
     'limpa': '\033[m',
-    'bold': '\033[1n',
+    'bold': '\033[1m',
     'roxo': '\033[1;35m',
     'azul': '\033[34m',
     'amarelo': '\033[33m'
 }
 
 print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
-print('PAR OU ÍMPAR'.center(60))
+print(f'{forms["bold"]}PAR OU ÍMPAR{forms["limpa"]}'.center(60))
 print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 
 numeros = []
-
 par = []
-
 impar = []
 
 while True:
-    num = int(input('>>> Digite um número: '))
-    numeros.append(num)
-
-    if num % 2 == 0:
-        par.append(num)
-
-    else:
-        impar.append(num)
+    numeros.append(int(input('>>> Digite um número: ')))
 
     resp = str(input('>>> Adicionar mais um número? [S / N]: ')).strip().upper()[0]
 
@@ -41,10 +32,17 @@ while True:
     elif resp == 'N':
         break
 
-print('\n... Você digitou os valores: [ ', end='')
+print('\n... Você digitou os valores: [', end='')
 
 for item in range(len(numeros)):
-    print(f'{forms["azul"] if item % 2 == 0 else forms["amarelo"]}{numeros[item]}{forms["limpa"]}', end=', ' if item < len(numeros) - 1 else ' ] \n')
+    print(f'{forms["azul"] if item % 2 == 0 else forms["amarelo"]}{numeros[item]}{forms["limpa"]}', end=', ' if item < len(numeros) - 1 else '] \n')
+
+for i, v in enumerate(numeros):
+    if v % 2 == 0:
+        par.append(v)
+
+    else:
+        impar.append(v)
 
 print('-=-' * 20)
 print(f'... Os valores ímpares foram: {forms["amarelo"]}{impar}{forms["limpa"]}')
