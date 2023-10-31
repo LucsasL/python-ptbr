2bdbfc911fafa57969473949a4895e78f1c91aa2
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-10-31 11:50:00 -0300

    Desafio 80 finalmente concluído

--- desafios/mod3/desafio080.py
+++ desafios/mod3/desafio080.py
@@ -4,28 +4,27 @@ forms = {
     'roxo': '\033[1;35m',
 }
 
+print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
+print('ORGANIZADOR DE LISTA SEM USO DO SORT'.center(60))
+print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
+
 numeros = []
 
-print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
-print('Desafio 80'.center(60))
-print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
-
 for c in range(0, 5):
-    num = float(input(f'>>> Digite o {c + 1}° número número: '))
+    num = int(input(f'>>> Digite o {c + 1}° número número: '))
 
     if c == 0 or num > numeros[-1]:
         print('... Número adicionado na posição final da lista...')
         numeros.append(num)
     
     else:
-        if num < numeros[0]:
-            pos = 0
-            while pos < len(numeros):
-                if num <= numeros[pos]:
-                    print(f'... Número posicionado no índice {pos} da lista...')
-                    numeros.insert(pos, num)
-                    break
-                pos += 1
+        pos = 0
+        while pos < len(numeros):
+            if num <= numeros[pos]:
+                print(f'... Número posicionado no índice {pos} da lista...')
+                numeros.insert(pos, num)
+                break
+            pos += 1
 
 print('-=-' * 30)
-print(f'... Os valores digitados em ordem foi {numeros:.2f}.')
+print(f'... Os valores digitados em ordem foi {numeros}.')
