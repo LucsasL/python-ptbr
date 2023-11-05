7e09399b99bc2e95371a569aa0a6198563beb16f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-05 19:10:09 -0300

    Update desafio086..py

--- desafios/mod3/desafio086..py
+++ desafios/mod3/desafio086..py
@@ -8,17 +8,14 @@ print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 print(f'{forms["bold"]}Matrizes em Python{forms["limpa"]}'.center(60))
 print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 
-matrix = []
+matriz = [[], [], []]
 
 for l in range(0, 3):
-    lin = []
-
     for c in range(0, 3):
-        lin.append(str(input(f'>>> Digite um valora para a posição [{l}, {c}]: ')).strip())
-    matrix.append(lin[:])
+        matriz[l].append(str(input(f'>>> Digite um valora para a posição [{l}, {c}]: ')).strip())
 
 print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
 
-for c in range(0, 3):
-    for i in range(0, 3):
-        print(f'[ {matrix[c][i]} ]', end='' if i < 2 else '\n')
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[ {matriz[l][c]:^5} ]', end='' if c < 2 else '\n')
