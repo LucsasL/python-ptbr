0bdffd8980dcce2695529ac87561b9f6a4de4d30
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-06 17:38:10 -0300

    Desafio 89 completo e com adições a mais

--- desafios/mod3/desafio089.py
+++ desafios/mod3/desafio089.py
@@ -39,8 +39,65 @@ for i in alunos: # Erro ao tentar captar valor em uma sub-lista dentro de outra
     media.append(calcMedia / 2)
     calcMedia = 0
 
+print()
 print(f' BOLETIM '.center(60, '='))
+print(f'{"N.":3}{"NOME":50}{"MÉDIA"}')
 
-for m in media:
-    print(f'{alunos[c][0]:50}{media[c]}')
+print('-' * 60)
+
+for i, m in enumerate(media):
+    print(f'{i:<3}{alunos[c][0]:50}{media[c]}')
     c += 1
+
+print('-' * 60)
+
+print(f'>>> (999 para interromper programa) <<<')
+
+while True:
+    if len(alunos) == 1:
+        mostra = str(input(f'>>> Mostrar notas do aluno? [S / N]: ')).strip().upper()[0]
+
+        if mostra != 'S' and mostra != 'N':
+            while True:
+                mostra = str(input('>>> Opção inválida. Digite novamente [S / N]: ')).strip().upper()[0]
+
+                if mostra == 'S' or mostra == 'N':
+                    break
+        
+            if mostra == 'N':
+                print('... FINALIZANDO...')
+                break
+
+        elif mostra == 'S':
+            print(f'... Notas de {alunos[0][0]} são [{alunos[0][1]} {alunos[0][2]}]\n')
+            break
+
+        elif mostra == 'N':
+            print('... FINALIZANDO...')
+            break
+
+    else:
+        mostra = int(input(f'>>> Mostrar notas de qual aluno? [0 a {len(alunos) - 1}]: '))
+
+        if mostra == 999:
+            print('... FINALIZANDO...')
+            break
+
+        elif 0 <= mostra <= len(alunos):
+            print(f'... Notas de {alunos[mostra][0]} são [{alunos[mostra][1]} {alunos[mostra][2]}]\n')
+
+        else:
+            while True:
+                print('>>> (999 para interromper) <<<')
+                mostra = int(input(f'>>> Índice inválido! Digite novamente [0 a {len(alunos) - 1}]: '))
+
+                if 0 <= mostra <= len(alunos):
+                    print(f'... Notas de {alunos[mostra][0]} são [{alunos[mostra][1]} {alunos[mostra][2]}]\n')
+                    break
+
+                elif mostra == '999':
+                    print('... FINALIZANDO...')
+                    break
+
+            if mostra == 'N':
+                break
