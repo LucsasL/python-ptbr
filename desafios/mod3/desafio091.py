1001953cc79ad9ccf7f3475f536d8058d01d76bc
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-13 16:37:58 -0300

    Update desafio091.py

--- desafios/mod3/desafio091.py
+++ desafios/mod3/desafio091.py
@@ -1,5 +1,6 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 
 forms = {
     'limpa': '\033[m',
@@ -11,22 +12,23 @@ print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 print(f'{forms["bold"]}JOGAR DADOS{forms["limpa"]}'.center(60))
 print(f'{forms["roxo"]}-=-{forms["limpa"]}' * 20)
 
-jogadas = {}
+jogadas = {
+    'Jogador 1': randint(1, 6),
+    'Jogador 2': randint(1, 6),
+    'Jogador 3': randint(1, 6),
+    'Jogador 4': randint(1, 6)
+}
 
-pos = 1
+ranking = []
 
-for c in range(4):
-    jogadas[f'Jogada{c + 1}'] = (randint(1, 6))
+for i, k in jogadas.items():
+    print(f'{i} tirou {k} no dado.')
+    sleep(1)
 
-    if c > 0 and jogadas[f'Jogada{c + 1}'] > jogadas[f'Jogada{c}']:
-        print()
-
-print(jogadas)
-
-print('-=' * 30)
 print(' Ranking dos jogadores '.center(60, '='))
 
-for k, j in jogadas.items():
-    print(f'{pos}° lugar: {k} com {j}.')
-    pos += 1
+ranking = sorted(jogadas.items(), key=itemgetter(1), reverse=True)
+
+for i, k in enumerate(ranking):
+    print(f'{i + 1}° lugar: {k[0]} com {k[1]}.')
     sleep(1)
