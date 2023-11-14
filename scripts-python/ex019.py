a198f8efb01daec193dd93b9894ef1d1c881f3b4
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-14 15:54:21 -0300

    Update ex019.py

--- scripts-python/ex019.py
+++ scripts-python/ex019.py
@@ -9,16 +9,27 @@ def Titulo(title):
     print(f'{title}'.center(60))
     print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
 
-def Soma(n1=0, n2=0):
-    print(f'... A = {n1}, B = {n2}')
-    s = n1 + n2
-    print(f'... A soma entre {n1} e {n2} é igual a {s}')
+def Soma(*num):
+    s = 0
+    for v in num:
+        s += v
+    print(s)
+
+def Dobra(lst):
+    for i in range(len(lst)):
+        lst[i] *= 2
 
 # Programa principal
 Titulo('Muito foda essas funções')
 Titulo('Ensinado em um curso')
 Titulo('Exercício curso em vídeo')
 
-Soma(4, 5)
-Soma(8, 9)
-Soma(1, 2)
+Soma(4, 5, 2, 3, 7)
+Soma(8, 9, 5)
+Soma(1, 2, 8, 4)
+
+Valores = [5, 9, 4, 2, 3]
+
+Dobra(Valores)
+
+print(Valores)
