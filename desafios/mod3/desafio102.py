d69e219846fbdc98f90206d1c77874c120aacc31
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-16 17:40:16 -0300

    Update desafio102.py

--- desafios/mod3/desafio102.py
+++ desafios/mod3/desafio102.py
@@ -16,12 +16,11 @@ def Fatorial(num, show = False):
     :param show: (opcional) Mostra operação de caculo.
     :return: O valor do fatorial do número "num".
     '''
-    count = num
     if show == True:
         for c in range(num, 0, -1):
             print(f'{c} x ' if c > 1 else f'{c} = ', end='')
 
-    for c in range(count - 1, 1, -1):
+    for c in range(num - 1, 1, -1):
         num *= c
 
     return num
