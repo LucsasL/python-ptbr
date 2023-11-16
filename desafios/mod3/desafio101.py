bed1b0fb8b57724428b3f2327dec6485da96de34
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-16 17:23:36 -0300

    Update desafio101.py

--- desafios/mod3/desafio101.py
+++ desafios/mod3/desafio101.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 forms = {
     'limpa': '\033[m',
     'bold': '\033[1m',
@@ -13,30 +11,26 @@ def Titulo(title):
     print(f'{forms["bold"]}{title}{forms["limpa"]}'.center(60))
     print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
 
-def CalcIdade(AnoNasc):
+def Voto(AnoNasc):
     '''
     -> Retorna Condição de voto nas eleições baseado na idade do indivíduo.
     :Param AnoNasc: Ano de nascimento do cidadão
     :Return: Retorna condição
     '''
+    from datetime import date
     AnoAtual = date.today().year
     idade = AnoAtual - AnoNasc
-    return idade
 
-def Condição():
     if idade < 16:
-        return f'{forms["vermelho"]}NEGADO.{forms["limpa"]}'
+        return f'... Com {idade} anos o direito ao voto é {forms["vermelho"]}NEGADO.{forms["limpa"]}'
     
     elif idade < 18 or idade > 65:
-        return f'{forms["verde"]}OPCIONAL.{forms["limpa"]}'
+        return f'... Com {idade} anos o direito ao voto é {forms["verde"]}OPCIONAL.{forms["limpa"]}'
     
     else:
-        return f'{forms["vermelho"]}OBRIGATÓRIO.{forms["limpa"]}'
+        return f'... Com {idade} anos o dever/direito do voto é {forms["vermelho"]}OBRIGATÓRIO.{forms["limpa"]}'
 
 # Programa Principal
 Titulo('CONDIÇÃO DE VOTO')
 
-idade = CalcIdade(int(input('>>> Digite seu ano de nascimento: ')))
-Cond = Condição()
-
-print(f'... Com {idade} anos, a condição de voto é {Cond}')
+print(Voto(int(input('>>> Digite seu ano de nascimento: '))))
