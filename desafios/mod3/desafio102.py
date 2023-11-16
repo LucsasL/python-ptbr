ea9dddb3986935726f41a3ad0f84f6665374365d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-16 15:40:37 -0300

    Create desafio102.py

---
+++ desafios/mod3/desafio102.py
@@ -0,0 +1,35 @@
+forms = {
+    'limpa': '\033[m',
+    'bold': '\033[1m',
+    'roxo': '\033[1;35m',
+}
+
+def Titulo(title):
+    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
+    print(f'{forms["bold"]}{title}{forms["limpa"]}'.center(60))
+    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
+
+def Fatorial(num, show = False):
+    '''
+    -> Calcula o fatorial de um número.
+    :param num: Número a ser calculado.
+    :param show: (opcional) Mostra operação de caculo.
+    :return: O valor do fatorial do número "num".
+    '''
+    for c in range(num - 1, 1, -1):
+        num *= c
+
+    if show == True:
+        for c in range(5, 0, -1):
+            print(f'{c} x ' if c > 1 else f'{c} = ', end='')
+    
+    return num
+
+# Programa Principal7
+Titulo('CALCULANDO FATORIAL ATRAVÉS DE FUNÇÕES')
+
+NumFator = Fatorial(5, True)
+
+print(NumFator)
+
+help(Fatorial)
