ccec4331a73920125394b47f77daef96660899e6
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-18 19:03:54 -0300

    Update desafio106.py

--- desafios/mod3/desafio106.py
+++ desafios/mod3/desafio106.py
@@ -1,33 +1,34 @@
 # Declaração de variáveis
 resp = ''
 
-forms = {
-    'limpa': '\033[m',
-    'bold': '\033[1m',
-    'inverso': '\033[1;40m',
-    'roxo': '\033[1;35m',
-    'vermelho': '\033[31m'
-}
+forms = (
+    '\033[m',
+    '\033[1m',
+    '\033[1;40m',
+    '\033[1;35m',
+    '\033[31m'
+)
 
 # Funções
 def Titulo(title):
-    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
-    print(f'{forms["bold"]}{title}{forms["limpa"]}'.center(60))
-    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
+    print(f'{forms[3]}-={forms[0]}' * 30)
+    print(f'{forms[1]}{title}{forms[0]}'.center(60))
+    print(f'{forms[3]}-={forms[0]}' * 30)
 
-def Ajuda(doc):
-    print(f'{forms["inverso"]}')
+def Ajuda(doc, cor = forms[0]):
+    Titulo(f'ACESSANDO DOCUMENTAÇÃO "{doc}"')
+    print(cor, end='')
     help(doc)
-    print(f'{forms["limpa"]}')
+    print(forms[0])
 
 # Programa Principal
 Titulo('SISTEMA DE AJUDA PyHELP')
 
 while True:
-    resp = str(input(f'>>> Função ou Biblioteca {forms["vermelho"]}[FIM para finalizar]{forms["limpa"]}: ')).strip()
+    resp = str(input(f'>>> Função ou Biblioteca {forms[4]}[FIM para finalizar]{forms[0]}: ')).strip()
 
-    if resp not in 'FIMfimFim':
-        Ajuda(resp)
+    if resp.upper() != 'FIM':
+        Ajuda(resp, forms[2])
         print(resp)
     
     else:
