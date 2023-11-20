7c2c0a35e4333a9c9fa83b4f18ee3da92d736417
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-20 02:04:00 -0300

    Update ex021.py

--- scripts-python/ex021.py
+++ scripts-python/ex021.py
@@ -4,10 +4,19 @@ forms = {
     'roxo': '\033[1;35m'
 }
 
+num = 6
+
 def Titulo(title):
     print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
     print(f'{forms["bold"]}{title}{forms["limpa"]}'.center(60))
     print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
 
+def Fatorial(n):
+    for c in range(n - 1, 1, -1):
+        n *= c
+    return n
+
 # Programa Principal
 Titulo('MODULARIZAÇÃO EM PYTHON')
+fat = Fatorial(num)
+print(f'... O fatorial de {num} é igual a {fat}.')
