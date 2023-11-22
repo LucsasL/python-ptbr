86a9e974062f303f6c5ce9436736769a4b424062
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-22 06:46:20 -0300

    Desafio 110

--- desafios/mod3/desafio 110/pacote/moedas/__init__.py
+++ desafios/mod3/desafio 110/pacote/moedas/__init__.py
@@ -1,13 +1,17 @@
 from pacote import cores
 
 # Funções externas
-def Resumo(p, aum, dim):
+def formMoeda(p, form = True, moeda = 'R$'):
+    res = f'{moeda} {p:.2f}'.replace('.', ',')
+    return res if form is True else 'cu'
+
+def Resumo(p, aum, dim, form = True):
     print(f'{cores.Roxo()}-={cores.Padrão()}' * 30)
     print(f'RESUMO DO VALOR'.center(60))
     print(f'{cores.Roxo()}-{cores.Padrão()}' * 60)
-    print(f'{"Preço analisado:":<50}{f"R$ {p:.2f}":>5}')
-    print(f'{"Dobro do preço:":<50}{f"R$ {p * 2:.2f}":>5}')
-    print(f'{"Metade do preço":<50}{f"R$ {p / 2:.2f}":>5}')
-    print(f'{f"{aum}% de aumento:":<50}{f"R$ {p + (p * aum / 100):.2f}":>5}')
-    print(f'{f"{dim}% de redução:":<50}{f"R$ {p - (p * dim / 100):.2f}":>5}')
+    print(f'{"Preço analisado:":<50}{formMoeda(p):>}')
+    print(f'{"Dobro do preço:":<50}{formMoeda(p * 2):>}')
+    print(f'{"Metade do preço":<50}{formMoeda(p / 2):>}')
+    print(f'{f"{aum}% de aumento:":<50}{formMoeda(p + (p * aum / 100)):>}')
+    print(f'{f"{dim}% de redução:":<50}{formMoeda(p - (p * dim / 100)):>}')
     print(f'{cores.Roxo()}-={cores.Padrão()}' * 30)
