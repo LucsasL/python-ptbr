6c460543759ffc90c60739514b58ffd747948457
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2023-11-23 23:29:30 -0300

    Create desafio113.py

---
+++ desafios/mod3/desafio113.py
@@ -0,0 +1,32 @@
+forms = {
+    'limpa': '\033[m',
+    'bold': '\033[1m',
+    'roxo': '\033[1;35m',
+    'vermelho': '\033[31m'
+}
+
+def Titulo(title):
+    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
+    print(f'{forms["bold"]}{title}{forms["limpa"]}'.center(60))
+    print(f'{forms["roxo"]}-={forms["limpa"]}' * 30)
+
+def LeiaInt(msg):
+    n = msg
+    while True:
+        n = str(input(msg))
+
+        if n.isnumeric():
+            valor = int(n)
+            break
+
+        else:
+            print(f'{forms["vermelho"]}[ERRO] Digite um número inteiro válido.{forms["limpa"]}')
+    
+    return valor
+        
+# Programa Principal
+Titulo('INPUT EM FUNÇÕES')
+
+num = LeiaInt('>>> Digite um números: ')
+
+print(f'>>> Você digitou o número {num}.')
